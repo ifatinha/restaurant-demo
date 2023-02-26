0c8b37ac421f33748b476972e620a681f00db78d
Fatima Ferreira <ifatinha99@gmail.com>
2023-02-26 14:58:30 -0300

    about page finish

--- js/script.js
+++ js/script.js
@@ -11,3 +11,24 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+/** activate questions item */
+function openQuestion(event) {
+   const question = event.currentTarget;
+   const control = question.getAttribute("aria-controls");
+   const response = document.getElementById(control);
+   response.classList.toggle("ativa");
+
+   if (response.classList.contains("ativa")) {
+      question.setAttribute("aria-expanded", "true");
+   } else {
+      question.setAttribute("aria-expanded", "false");
+   }
+}
+
+function eventQuestions(question) {
+   question.addEventListener("click", openQuestion);
+}
+
+const questions = document.querySelectorAll(".faqs-questions button");
+questions.forEach(eventQuestions);
